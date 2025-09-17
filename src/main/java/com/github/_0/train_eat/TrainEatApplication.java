3589ac8840da02daa0f63ea8c90407a6c05c2fac
0-21-0 <468335336@qq.com>
2025-09-17 08:56:41 +0800

    创建新项目并连接数据库

---
+++ src/main/java/com/github/_0/train_eat/TrainEatApplication.java
@@ -0,0 +1,13 @@
+package com.github._0.train_eat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainEatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainEatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/_0/train_eat/TrainEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github._0.train_eat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainEatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
